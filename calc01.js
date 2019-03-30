f3d5917082a07c08904b1779ab0704be8a361ae0
Thomas Martinez <thomasmartinez.student@careerdevs.com>
2019-03-30 15:12:41 +0000

    Basic calculation of phone sales

---
+++ calc01.js
@@ -0,0 +1,11 @@
+var product1 = "iPhone X";
+var product2 = "Samsung 10";
+var product1Price = 1050;
+var product2Price = 950;
+var product1TotalSales = 75;
+var product2TotalSales = 65;
+
+var product1TotalSalesAmt = product1Price * product1TotalSales;
+var product2TotalSalesAmt = product2Price * product2TotalSales;
+
+console.log("Total sales for", product1, "is $" + product1TotalSalesAmt);
